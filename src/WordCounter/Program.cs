090da52ebfc67c310212a89c29e143e13f7e874d
pkmetski <plamen.kmetski@visma.com>
2023-12-09 21:31:49 +0100

    Naive implementation

---
+++ src/WordCounter/Program.cs
@@ -0,0 +1,38 @@
+﻿var filePaths = new[]
+{
+    "/Users/plamen.kmetski/file1.txt",
+    "/Users/plamen.kmetski/file2.txt"
+};
+
+var wordCounts = new Dictionary<string, int>();
+
+CountWords(filePaths, wordCounts);
+PrintCounted(wordCounts);
+
+static void CountWords(string[] filePaths, IDictionary<string, int> wordCounts)
+{
+    foreach (var filePath in filePaths)
+    {
+        var lines = File.ReadLines(filePath);
+        foreach (var line in lines)
+        {
+            var words = line.Split(" ");
+            foreach (var word in words)
+            {
+                if (!wordCounts.ContainsKey(word))
+                {
+                    wordCounts.Add(word, 0);
+                }
+                wordCounts[word]++;
+            }
+        }
+    }
+}
+
+static void PrintCounted(IDictionary<string, int> wordCounts)
+{
+    foreach (var wordCount in wordCounts)
+    {
+        Console.WriteLine($"{wordCount.Value}: {wordCount.Key}");
+    }
+}
